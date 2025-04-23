7c3036f4e87622febfebbc868ee179f8c5f43387
Elver581 <elveral100@gmail.com>
2025-04-23 17:14:21 -0500

    pqrcambios

--- app/Notifications/pqrNotifycaciones.php
+++ app/Notifications/pqrNotifycaciones.php
@@ -45,7 +45,7 @@ class pqrNotifycaciones extends Notification
             return (new MailMessage)
                 ->subject('Nueva PQR Recibida')
                 ->line('Se ha recibido una nueva PQR.')
-                ->line('Tipo: ' . $this->pqr['tipo'])
+            
                 ->line('Mensaje: ' . $this->pqr['mensaje'])
                 ->line('Nombre : ' . $this->pqr['nombre'])
                 ->line('Empresa: ' . $this->pqr['empresa'])
@@ -54,12 +54,21 @@ class pqrNotifycaciones extends Notification
                 ->line('Por favor, revisa la solicitud.');
         } else {
             return (new MailMessage)
-                ->subject('Confirmación de PQR')
-                ->line('Hemos recibido tu solicitud con los siguientes detalles:')
-                ->line('Tipo: ' . $this->pqr['tipo'])
-                ->line('Mensaje: ' . $this->pqr['mensaje'])
-                ->line('Nos pondremos en contacto contigo pronto.')
-                ->line('Gracias por comunicarte con nosotros.');
+            ->subject('Confirmación de recepción de PQR')
+            ->line('Estimado/a Cliente,')
+            ->line('Le agradecemos por contactarnos. Confirmamos la recepción de su solicitud, la cual contiene los siguientes detalles:')
+            ->line('')
+            ->line('📩 *Mensaje:* ' . $this->pqr['mensaje'])
+            ->line('')
+            ->line('Nuestro equipo de soporte ha iniciado la revisión de la información proporcionada. En un plazo de hasta 15 días hábiles nos comunicaremos con usted para informarle los próximos pasos o, si es necesario, solicitar información adicional.')
+            ->line('')
+            ->line('Si su solicitud requiere atención prioritaria, puede comunicarse con nuestra línea de atención al cliente al [Número de Teléfono].')
+            ->line('')
+            ->line('Agradecemos su paciencia y la confianza depositada en nosotros.')
+            ->line('')
+            ->line('Atentamente,')
+            ->line('El equipo de soporte de SETASPLAST SAS BIC');
+        
         }
     }
 
