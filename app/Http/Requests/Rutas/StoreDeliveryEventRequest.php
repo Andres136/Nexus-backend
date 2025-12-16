b6048cdd04a06581be376fd9749496b4086267be
Elver581 <elveral100@gmail.com>
2025-12-16 09:42:24 -0500

    Actualizar request ot entregas delivery

--- app/Http/Requests/Rutas/StoreDeliveryEventRequest.php
+++ app/Http/Requests/Rutas/StoreDeliveryEventRequest.php
@@ -22,7 +22,7 @@ class StoreDeliveryEventRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'orden_id' => 'required|exists:orden__compras,id',
+            'orden_id' => 'required|exists:ordenes_trabajo,id',
             'fecha_entrega' => 'required|date',
             'hora' => 'required|date_format:H:i',
             'usuario_id' => 'required|exists:users,id',
