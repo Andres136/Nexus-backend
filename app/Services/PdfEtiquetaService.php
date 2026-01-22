be481da8c6c7d9c60893593390f6f9adb94079e7
Elver581 <elveral100@gmail.com>
2026-01-22 10:55:24 -0500

    Ajustar el service de pdfEtiquetasService

--- app/Services/PdfEtiquetaService.php
+++ app/Services/PdfEtiquetaService.php
@@ -13,7 +13,7 @@ class PdfEtiquetaService
         $data = $productos->map(function ($p) use ($barcode) {
 
             // 🔴 SIEMPRE URL COMPLETA
-            $url = url('/s/' . $p->code);
+            $url = url('/' . $p->code);
 
             return [
                 'name' => $p->name,
