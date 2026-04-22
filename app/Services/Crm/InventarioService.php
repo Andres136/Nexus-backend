bcf468339a506f6c8cfd05657c6de73c5aab45df
Elver <elveral100@gmail.com>
2026-04-21 19:13:40 -0500

    Comentar linea en Inventario servise para descontar stock en cantidad_ejecutad_kg

--- app/Services/Crm/InventarioService.php
+++ app/Services/Crm/InventarioService.php
@@ -749,11 +749,12 @@ if (!$movimiento) {
             ]);
 
             $faltante = max(0, $cantidadTotal - $cantidadCubierta);
-if ($detalleId && $cantidadCubierta > 0) {
+
+/*if ($detalleId && $cantidadCubierta > 0) {
     Orden_Compra_Detalle::where('id', $detalleId)
         ->increment('cantidad_ejecutada_kg', $cantidadCubierta);
 }
-
+*/
             $resultados[] = [
                 'producto_id'        => $productoId,
                 'cantidad_requerida' => $cantidadTotal,
